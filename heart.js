65f232deb6e7d40ebed894afcfadffc69eb7fba7
huyquoccc <112641512+huyquoccc@users.noreply.github.com>
2022-11-11 12:47:19 +0700

    heart.file

---
+++ heart.js
@@ -0,0 +1,13 @@
+var canvas = document.querySelector("canvas");
+console.log(canvas);
+var after = document.querySelector("#afterText");
+function renderHeart(a) {
+
+
+     confirm("Do you want open ??")
+     a.classList.add("active");
+     after.classList.add("active");
+    canvas.classList.add("active");
+
+
+}
